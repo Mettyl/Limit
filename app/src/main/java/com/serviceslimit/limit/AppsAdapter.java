2f9a5fc1b52c2375cfe1f83f99be33ff168f0b5a
Mety <matej.sokol@student.gyarab.cz>
2018-04-16 21:52:28 +0200

    Basics

---
+++ app/src/main/java/com/serviceslimit/limit/AppsAdapter.java
@@ -0,0 +1,63 @@
+package com.serviceslimit.limit;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AppsAdapter extends RecyclerView.Adapter<AppsAdapter.AppHolder> {
+
+    private Context context;
+    private PackageManager packageManager;
+    private List<ApplicationInfo> infoList;
+
+    public AppsAdapter(Context context, List<ApplicationInfo> infoList,PackageManager packageManager){
+        this.context = context;
+        this.infoList = infoList;
+        this.packageManager = packageManager;
+    }
+
+    @NonNull
+    @Override
+    public AppHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.app_recycler_row, parent, false);
+
+        return new AppHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppHolder holder, int position) {
+
+        ApplicationInfo info = infoList.get(position);
+
+        holder.icon.setImageDrawable(info.loadIcon(packageManager));
+        holder.name.setText((String) packageManager.getApplicationLabel(info));
+    }
+
+    @Override
+    public int getItemCount() {
+        return infoList.size();
+    }
+
+
+    class AppHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private ImageView icon;
+
+        public AppHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.app_recycler_name_tv);
+            icon = itemView.findViewById(R.id.app_recycler_icon_iv);
+        }
+    }
+}
--- app/src/main/java/com/serviceslimit/limit/MainActivity.java
+++ app/src/main/java/com/serviceslimit/limit/MainActivity.java
@@ -1,7 +1,22 @@
 package com.serviceslimit.limit;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Intent intent = new Intent();
+//        intent.setAction(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        Uri uri = Uri.fromParts("package", getPackageName(), null);
+//        intent.setData(uri);
+//        startActivity(intent);
+//
+
+        PackageManager packageManager = null;
+        List<ApplicationInfo> apps = new ArrayList<>();
+        try {
+            packageManager = getPackageManager();
+            if (packageManager != null) {
+                List<PackageInfo> packs = packageManager.getInstalledPackages(0);
+                for (int i = 0; i < packs.size(); i++) {
+                    PackageInfo p = packs.get(i);
+                    ApplicationInfo a = p.applicationInfo;
+
+                    if ((a.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
+                        continue;
+                    }
+                    apps.add(a);
+                }
+            }
+        } catch (Exception e1) {
+            Log.i("debug", "error: ", e1);
+        }
+
+
+        RecyclerView recyclerView = findViewById(R.id.apps_reycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new AppsAdapter(getApplicationContext(), apps, packageManager));
     }
 }
