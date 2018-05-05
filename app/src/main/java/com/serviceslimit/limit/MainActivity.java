8e4121919c4cf487904ea72c06e50018bea5573f
Jan Unar <johnunar@gmail.com>
2018-05-05 22:23:58 +0200

    Added login function

--- app/src/main/java/com/serviceslimit/limit/MainActivity.java
+++ app/src/main/java/com/serviceslimit/limit/MainActivity.java
@@ -1,21 +1,16 @@
 package com.serviceslimit.limit;
 
-import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
 //        Intent intent = new Intent();
 //        intent.setAction(Settings.ACTION_USAGE_ACCESS_SETTINGS);
